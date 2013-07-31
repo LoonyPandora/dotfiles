f03c84878174657c1f305384b24959a601a015ee
James Aitken <loonypandora@gmail.com>
2013-07-31 17:08:43 +0100

    Add my public key automatically

--- setup.sh
+++ setup.sh
@@ -3,6 +3,11 @@
 # Script that will symlink all stuff necessary - overwriting most files
 # Will only ADD to the authorized_keys however
 
+# You MUST clone the repo into your homedir, and call it ".dotfiles"
+
+# Add authorized_keys
+cat "$HOME/.dotfiles/config/ssh/authorized_keys" >> "$HOME/.ssh/authorized_keys"
+
 # TODO: Make this read all files from the config dir with some error handling
 dotfiles="bash_aliases bash_completion bash_profile bashrc hushlogin jsbeautifyrc jshintrc perltidyrc"
 
