684e20d03104c2ad27cffe9ce4c372a2662001d8
James Aitken <loonypandora@gmail.com>
2013-07-31 16:56:55 +0100

    Improve setup script

--- setup.sh
+++ setup.sh
@@ -3,19 +3,10 @@
 # Script that will symlink all stuff necessary - overwriting most files
 # Will only ADD to the authorized_keys however
 
-# MUST be run from a home directory
-
-
-
 # TODO: Make this read all files from the config dir with some error handling
-
 dotfiles="bash_aliases bash_completion bash_profile bashrc hushlogin jsbeautifyrc jshintrc perltidyrc"
 
 for dotfile in $dotfiles
 do
-    ln -sf "$HOME/.dotfiles/$dotfile" "$HOME/.$dotfile"
+    ln -sf "$HOME/.dotfiles/config/$dotfile" "$HOME/.$dotfile"
 done
-
-
-
-# echo $CONFIG_DIR;
